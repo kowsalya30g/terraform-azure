469421b3446a3d9c88c443eb8eeb910c2f1eff11
kowsalya30g <kousigowthaman99@gmail.com>
2025-08-24 12:30:00 +0530

    added new comment lines

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
-
+# create aruntf file 
 # Create a resource group using the generated random name
 resource "azurerm_resource_group" "example" {
   location = var.resource_group_location
